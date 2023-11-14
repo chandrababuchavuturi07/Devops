cad308687bb39b51aad06457be9e11776dc997c5
chandrababuchavuturi07 <125848589+chandrababuchavuturi07@users.noreply.github.com>
2023-11-14 10:52:17 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# Function to calculate the square of a number
+def calculate_square(number):
+    return number * number
+
+# Sample usage of the function
+input_number = 5
+result = calculate_square(input_number)
+print(f"The square of {input_number} is {result}")
